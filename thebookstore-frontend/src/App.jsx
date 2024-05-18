b70ed7d79ba80d11bbf8b6eea7c34b7774f8fe48
Achira Wijesuriya <wijesuriya4me@gmail.com>
2024-05-19 03:34:25 +0530

    add some changes
    add basic file structure

--- thebookstore-frontend/src/App.jsx
+++ thebookstore-frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import { Outlet } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,8 +8,9 @@ function App() {
   return (
     <>
       <div>
-        Hello world !
+        Hello world!
       </div>
+      <Outlet/>
     </>
   )
 }
--- thebookstore-frontend/src/routers/router.jsx
+++ thebookstore-frontend/src/routers/router.jsx
@@ -2,12 +2,35 @@ import {
     createBrowserRouter,
     RouterProvider,
 } from "react-router-dom";
+import App from "../App";
+import Home from "../home/Home";
+import Shop from "../home/Shop";
+import About from "../home/About";
+import Login from "../home/Login";
 
 const router = createBrowserRouter(
     [
         {
             path: "/",
-            element: <div>Hello world!</div>,
+            element: <App/>,
+            children: [
+                {
+                    path: "/",
+                    element: <Home/>
+                },
+                {
+                    path: "/shop",
+                    element: <Shop/>
+                },
+                {
+                    path: "/about",
+                    element: <About/>
+                },
+                {
+                    path: "/login",
+                    element: <Login/>
+                }
+            ]
         },
     ]
 );
