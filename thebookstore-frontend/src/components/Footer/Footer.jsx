6fad9f23d8699fb72ce3b36f7c3875a0db08ffed
Achira Wijesuriya <wijesuriya4me@gmail.com>
2024-06-02 23:48:23 +0530

    update footer

---
+++ thebookstore-frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="bg-zinc-800 text-white  px-8 py-4">
+        <p className="font-semibold text-center">
+          &copy; 2024 | 
+          <a href="https://arwijesuriya.vercel.app/" target='_blank'> 2020ICT101</a>
+        </p>
+    </div>
+  )
+}
+
+export default Footer
